73d82f8ce0d2aa141afb7c96ac173b3ec68eb3b3
jeffreyvh <jeffrey25011998@gmail.com>
2015-06-02 07:55:40 +0200

    Update common_tablet.mk
    Use vendor/coderom instead.

--- config/common_tablet.mk
+++ config/common_tablet.mk
@@ -1,8 +1,8 @@
 # Common settings and files
--include vendor/omni/config/common.mk
+-include vendor/coderom/config/common.mk
 
 # Add tablet overlays
-PRODUCT_PACKAGE_OVERLAYS += vendor/omni/overlay/common_tablet
+PRODUCT_PACKAGE_OVERLAYS += vendor/coderom/overlay/common_tablet
 
 PRODUCT_CHARACTERISTICS := tablet
 
