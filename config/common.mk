7a7101f87899606fe965e0937245bd2233bb0000
jeffreyvh <jeffrey25011998@gmail.com>
2015-06-02 07:54:45 +0200

    Update common.mk
    Use vendor/coderom instead.

--- config/common.mk
+++ config/common.mk
@@ -1,12 +1,12 @@
-PRODUCT_BRAND ?= omni
+PRODUCT_BRAND ?= coderom
 
 # use specific resolution for bootanimation
 ifneq ($(TARGET_BOOTANIMATION_SIZE),)
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/bootanimation/res/$(TARGET_BOOTANIMATION_SIZE).zip:system/media/bootanimation.zip
+    vendor/coderom/prebuilt/bootanimation/res/$(TARGET_BOOTANIMATION_SIZE).zip:system/media/bootanimation.zip
 else
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+    vendor/coderom/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip
 endif
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
@@ -31,25 +31,25 @@ endif
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh \
-    vendor/omni/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
-    vendor/omni/prebuilt/bin/50-hosts.sh:system/addon.d/50-hosts.sh \
-    vendor/omni/prebuilt/bin/blacklist:system/addon.d/blacklist 
+    vendor/coderom/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh \
+    vendor/coderom/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/coderom/prebuilt/bin/50-hosts.sh:system/addon.d/50-hosts.sh \
+    vendor/coderom/prebuilt/bin/blacklist:system/addon.d/blacklist 
     
     
 
 # init.d support
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/omni/prebuilt/bin/sysinit:system/bin/sysinit
+    vendor/coderom/prebuilt/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/coderom/prebuilt/bin/sysinit:system/bin/sysinit
 
 # userinit support
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/etc/init.d/90userinit:system/etc/init.d/90userinit
+    vendor/coderom/prebuilt/etc/init.d/90userinit:system/etc/init.d/90userinit
 
 # Init script file with omni extras
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/etc/init.local.rc:root/init.omni.rc
+    vendor/coderom/prebuilt/etc/init.local.rc:root/init.omni.rc
 
 # Enable SIP and VoIP on all targets
 PRODUCT_COPY_FILES += \
@@ -62,21 +62,21 @@ endif
 
 # Dashclock
 #PRODUCT_COPY_FILES += \
-#    vendor/omni/prebuilt/app/DashClock.apk:system/app/DashClock.apk
+#    vendor/coderom/prebuilt/app/DashClock.apk:system/app/DashClock.apk
 
 # NovaLauncher
 PRODUCT_COPY_FILES += \
-	vendor/omni/prebuilt/app/NovaLauncher.apk:system/priv-app/NovaLauncher/NovaLauncher.apk
+	vendor/coderom/prebuilt/app/NovaLauncher.apk:system/priv-app/NovaLauncher/NovaLauncher.apk
 	
 # Shuttle
 PRODUCT_COPY_FILES += \
-	vendor/omni/prebuilt/app/Shuttle.apk:system/app/Shuttle/Shuttle.apk
+	vendor/coderom/prebuilt/app/Shuttle.apk:system/app/Shuttle/Shuttle.apk
 
 # Additional packages
--include vendor/omni/config/packages.mk
+-include vendor/coderom/config/packages.mk
 
 # Versioning
--include vendor/omni/config/version.mk
+-include vendor/coderom/config/version.mk
 
 # Add our overlays
-PRODUCT_PACKAGE_OVERLAYS += vendor/omni/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/coderom/overlay/common
