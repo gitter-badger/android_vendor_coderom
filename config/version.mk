75942789434ba0901a6fcfe1b8a47cb67564f894
jeffreyvh <jeffrey25011998@gmail.com>
2015-06-02 07:58:17 +0200

    Update version.mk
    Use vendor/coderom instead.

--- config/version.mk
+++ config/version.mk
@@ -18,7 +18,7 @@ ifndef ROM_BUILDTYPE
 endif
 
 TARGET_PRODUCT_SHORT := $(TARGET_PRODUCT)
-TARGET_PRODUCT_SHORT := $(subst omni_,,$(TARGET_PRODUCT_SHORT))
+TARGET_PRODUCT_SHORT := $(subst coderom_,,$(TARGET_PRODUCT_SHORT))
 
 # Build the final version string
 ifdef BUILDTYPE_RELEASE
@@ -33,5 +33,5 @@ endif
 
 # Apply it to build.prop
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.modversion=OmniROM-$(ROM_VERSION) \
-    ro.omni.version=$(ROM_VERSION)
+    ro.modversion=CodeROM-$(ROM_VERSION) \
+    ro.coderom.version=$(ROM_VERSION)
